2253e71108521d0b0d52f61d46d65e6acb9ffc7d
Koustav <bkoustav361@gmail.com>
2026-05-24 12:31:38 +0530

    var upd

--- var.py
+++ var.py
@@ -18,6 +18,9 @@ print(f"your id is {id}");#{} is a placeholder for the variable id
 weight = 70.5
 print(f"your weight is {weight}");
 #these are booleans
-is_adult = age >= 18
+is_adult = age >= 30
 print(f"are you an adult? {is_adult}");
-
+if is_adult:
+    print("you are an adult");
+else:    
+    print("you are not an adult");
