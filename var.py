2233789d1d6c06ad3380085f58f33a414ab3745e
Koustav <bkoustav361@gmail.com>
2026-05-24 12:18:48 +0530

    var

---
+++ var.py
@@ -0,0 +1,15 @@
+#string,integer,float,boolean
+name = "koustav"
+age = 20
+height = 5.9
+is_student = True
+print(name)
+print(age)
+print(height)
+print(is_student)
+#these are strings
+firt_name="Koustav"
+print(f"hello {firt_name}");
+#these are integers
+id=10
+print("your id is",id);
