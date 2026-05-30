87643aa74340a5cbaee5a8385ca915e6661a8586
Koustav <bkoustav361@gmail.com>
2026-05-30 21:01:03 +0530

    arithmatic

---
+++ arithmatic.py
@@ -0,0 +1,17 @@
+friends=1
+friends+=1#augmented assignment operator
+print(friends)
+friends-=2
+print(friends)
+friends*=3
+print(friends)
+friends/=2
+print(friends)
+friends1=3
+friends1//=2 #floor division operator
+print(friends1)
+friends**=2 #exponentiation operator
+print(friends)
+rem=1
+rem%=2 #modulus operator
+print(rem)
